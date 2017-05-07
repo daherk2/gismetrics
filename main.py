958ecb9d083234e2f6b35c57457649f475a1eb5d
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2017-05-07 12:04:08 -0300

    Insercao do conversor bussola.

--- main.py
+++ main.py
@@ -21,5 +21,17 @@ def Haversine(ponto1, ponto2, medida = 'metro'):
     if medida == 'pes':
         return h * 3280.84
 
+def moviment(center, nstep):
+    if center[0] > nstep[0] and center[1] < nstep[1]:
+        return 'S'
+    if center[0] < nstep[0] and center[1] > nstep[1]:
+        return 'N'
+    if center[0] < nstep[0] and center[1] < nstep[1]:
+        return 'E'
+    if center[0] > nstep[0] and center[1] > nstep[1]:
+        return 'W'
+
+
+#print moviment([-22.539538, -44.772836], [-22.539957, -44.773748])
 #print Haversine([-22.540540, -44.773176],[-22.540687, -44.773793])
 
